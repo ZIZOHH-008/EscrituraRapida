a2cb186240cc7951907e20bdf37aac5dcf4fe9b4
zizohh <parrapradoj@gmail.com>
2026-09-24 15:25:57 -0500

    New function: "Init" game

--- src/main/java/org/example/escriturarapida/HelloController.java
+++ src/main/java/org/example/escriturarapida/HelloController.java
@@ -1,14 +1,30 @@
 package org.example.escriturarapida;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void cambiarPantalla(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(
+                getClass().getResource("basicGame.fxml")
+        );
+
+        Parent root = loader.load();
+
+        Stage stage = (Stage) ((Node) event.getSource())
+                .getScene()
+                .getWindow();
+
+        stage.setScene(new Scene(root));
     }
 }
